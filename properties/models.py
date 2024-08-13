afd4cbd5b872ce77f2b2326ec627cb7339af7e8f
vincenttommi <vincenttommi@gmail.com>
2024-08-13 12:59:08 +0300

    properties models

--- properties/models.py
+++ properties/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from  users.models import Landlord
 
-# Create your models here.
+
+
+
+
+
+
+
+class Property(models.Model):
+
+
+   NAME_CHOICES =[
+   ('Apartment','Apartment'),
+   ('House','House'),
+   ('Condo','Condo'),
+   ]
+
+
+
+
+
+
+   STATUS_CHOICES = [
+       (0, 'Available'),
+       (1, 'Not available'),
+   ]
+
+
+
+
+   name = models.CharField(max_length=255)
+   location = models.CharField(max_length=200)
+   rooms  = models.IntegerField()
+   price = models.IntegerField()
+   type = models.CharField(max_length=50,choices=STATUS_CHOICES)
+   status = models.IntegerField(choices=STATUS_CHOICES)
+   landlord_id  = models.IntegerField(Landlord, on_delete=models.CASCADE) #The Foreignkey field in Property model references the Landlord model from users app
+
+
+
+
+
+
+   def __str__(self):
+       return f"{self.name} - {self.location}"
