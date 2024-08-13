f4e646fb0176770fd5910f4a5c06661badfd9b0a
vincenttommi <vincenttommi@gmail.com>
2024-08-13 14:05:07 +0300

    correcting database  connection

--- properties/models.py
+++ properties/models.py
@@ -1,47 +1,25 @@
 from django.db import models
-from  users.models import Landlord
-
-
-
-
-
-
-
+from users.models import Landlord  # Importing the Landlord model from the users app
 
 class Property(models.Model):
+    NAME_CHOICES = [
+        ('Apartment', 'Apartment'),
+        ('House', 'House'),
+        ('Condo', 'Condo'),
+    ]
+    
+    STATUS_CHOICES = [
+        (0, 'Available'),
+        (1, 'Not available'),
+    ]
+    
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=200)
+    rooms = models.IntegerField()
+    price = models.IntegerField()
+    type = models.CharField(max_length=50, choices=NAME_CHOICES)  
+    status = models.IntegerField(choices=STATUS_CHOICES)
+    landlord_id = models.ForeignKey(Landlord, on_delete=models.CASCADE)  
 
-
-   NAME_CHOICES =[
-   ('Apartment','Apartment'),
-   ('House','House'),
-   ('Condo','Condo'),
-   ]
-
-
-
-
-
-
-   STATUS_CHOICES = [
-       (0, 'Available'),
-       (1, 'Not available'),
-   ]
-
-
-
-
-   name = models.CharField(max_length=255)
-   location = models.CharField(max_length=200)
-   rooms  = models.IntegerField()
-   price = models.IntegerField()
-   type = models.CharField(max_length=50,choices=STATUS_CHOICES)
-   status = models.IntegerField(choices=STATUS_CHOICES)
-   landlord_id  = models.IntegerField(Landlord, on_delete=models.CASCADE) #The Foreignkey field in Property model references the Landlord model from users app
-
-
-
-
-
-
-   def __str__(self):
-       return f"{self.name} - {self.location}"
+    def __str__(self):
+        return f"{self.name} - {self.location}"
