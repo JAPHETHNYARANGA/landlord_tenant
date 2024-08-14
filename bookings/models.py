5b8259d88cc88c3878ca74d1c271da46a229afc7
vincenttommi <vincenttommi@gmail.com>
2024-08-14 09:33:10 +0300

    booking models completed

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from properties.models import Property
+from users.models import Tenant
 
-# Create your models here.
+
+
+
+class Booking(models.Model):
+
+    STATUS_CHOICES = [
+        ('Pending','Pending'),
+        ('Confirmed','Confirmed'),
+        ('Cancelled','Cancelled'),
+        ('Completed', 'Complted'),
+    ]
+
+
+
+    booking_id = models.AutoField(primary_key=True)
+    property_id  = models.ForeignKey(Property, on_delete=models.CASCADE)
+    tenant_id  = models.ForeignKey(Tenant, on_delete=models.CASCADE)
+    booking_date  = models.DateField()
+    status = models.CharField(max_length=20,choices=STATUS_CHOICES,default='Pending')
+
+    def __str_(self):
+        return f"Booking {self.booking_id}:{self.status}"
