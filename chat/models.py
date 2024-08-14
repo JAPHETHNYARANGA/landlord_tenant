0cf7da430d049a3f0693cbccaef1761e200bab2a
vincenttommi <vincenttommi@gmail.com>
2024-08-14 16:48:06 +0300

    chatbox models

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,14 @@
+from datetime import timezone
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Create your models here
+
+user_id  = models.ForeignKey(User, on_delete=models.CASCADE)
+message = models.TextField()
+timestamp = models.DateTimeField(default=timezone.now)
+
+
+
+def __str__(self):
+    return  f"{self.user.username} at {self.timestamp}:{self:message[:50]}"
