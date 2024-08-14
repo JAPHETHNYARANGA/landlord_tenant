6f9f66b6e32c719cfba9ea41ad99c2614ea79090
vincenttommi <vincenttommi@gmail.com>
2024-08-14 08:59:00 +0300

    completed maintenance models

--- maintenance/models.py
+++ maintenance/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from users.models import Tenant
+from properties.models import Property
 
-# Create your models here.
+
+
+
+class MaintenanceTicket(models.Model):
+   ISSUE_TYPE_CHOICES =[
+       ('Plumbing', 'Plumbing'),
+       ('Electrical','Electrical'),
+       ('Heating','Heating'),
+       ('Other', 'Other'),
+   ]
+
+
+   STATUS_CHOICES = [
+       ('Open','Open'),
+       ('In Progress','In Progress'),
+       ('Resolved', 'Resolved'),
+       ('Closed', 'Closed'),
+   ]
+
+
+   ticket_id = models.AutoField(primary_key=True)
+   tenant_id = models.ForeignKey(Tenant,on_delete=models.CASCADE )
+   property_id  = models.ForeignKey(Property, on_delete=models.CASCADE)
+   issue_type = models.CharField(max_length=50, choices=ISSUE_TYPE_CHOICES)
+   description = models.TextField()
+   status = models.CharField(max_length=20, choices=STATUS_CHOICES,default='Open')
+   released_date = models.DateField()
+   resolved_date = models.DateField(null=True, blank=True)
+
+
+   def __str__(self):
+       return  f"Ticket {self.ticket_id}:{self.issue_type}-{self.status}"
+  
+
+
+
+
+
