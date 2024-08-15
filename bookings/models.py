3cced03b7bf719c6eb920f93f2d33b5ce6f4ae29
vincenttommi <vincenttommi@gmail.com>
2024-08-15 13:18:58 +0300

    all models

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,27 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models  # Correct
+from properties.models import Property
+from users.models import Tenant
+
+
+
+
+class Booking(models.Model):
+
+    STATUS_CHOICES = [
+        ('Pending','Pending'),
+        ('Confirmed','Confirmed'),
+        ('Cancelled','Cancelled'),
+        ('Completed', 'Complted'),
+    ]
+
+
+
+    booking_id = models.AutoField(primary_key=True)
+    property_id  = models.ForeignKey(Property, on_delete=models.CASCADE)
+    tenant_id  = models.ForeignKey(Tenant, on_delete=models.CASCADE)
+    booking_date  = models.DateField()
+    status = models.CharField(max_length=20,choices=STATUS_CHOICES,default='Pending')
+
+    def __str_(self):
+        return f"Booking {self.booking_id}:{self.status}"
--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,9 @@
-from django.db import models
+from django.db import models 
 
-# Create your models here.
+class Chatbox(models.Model):
+    user_id = models.IntegerField()
+    message = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"User {self.user_id} at {self.timestamp}: {self.message[:50]}"
